2d2abc093c66cd72d39d959aef8971d4b1454fed
Igor Shabes <qrutas@gmail.com>
2016-06-23 16:10:43 +0300

    random stars input into board;

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
+import random
+
+
 class Board:
     board = []
 
-    def __init__(self, column, row):
-        for i in range(row):
-            self.board.append(['-']*column)
+    def __init__(self, rows, columns):
+        self.columns = columns
+        self.rows = rows
+        for row in range(self.rows):
+            self.board.append(['-'] * self.columns)
 
     def __repr__(self):
-        raw_board = ''
+        return "Board object: {}x{}".format(self.rows, self.columns)
+
+    def show(self):
+        print '~' * self.columns * 2
         for row in self.board:
-            for element in row:
-                raw_board += str(element)
-            raw_board += '\n'
-        return "This is your board:\n{}".format(raw_board)
+            print " ".join(row)
+        print '~' * self.columns * 2
+
+    def shuffle_stars(self):
+
+        for row in range(self.rows):
+            for column in range(self.columns):
+                if random.random() < 0.5:
+                    self.board[row][column] = '*'
 
 
-COLUMN = 10
-ROW = 5
-
-board = Board(COLUMN, ROW)
-print board
+board = Board(7, 7)
+print(board)
+board.shuffle_stars()
+board.show()
