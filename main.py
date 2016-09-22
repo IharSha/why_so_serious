e77a0c5680de2b6d63cdb9156d94f2d5241cd176
IharSha <qrutas@gmail.com>
2016-09-22 22:44:35 +0300

    Update main.py
    Restart game functionality added after the end;
    Map added;
    Difficulty reduced;
    Now on Python 3.4;

--- main.py
+++ main.py
@@ -2,89 +2,121 @@ import random
 import time
 
 
-class Dungeon:
-    """Create Dungeon object."""
-    board = []
-
-    def __init__(self, rows, columns):
-        self.columns = columns
-        self.rows = rows
-        for row in range(self.rows):
-            self.board.append(['-'] * self.columns)
-        for row in self.board:
-            print " ".join(row)
-        print('Here is your dungeon! Look, how good it is!\n')
-
-    def __repr__(self):
-        return "Board size {}x{}\n".format(self.rows, self.columns)
-
-    def show(self):
-        """reveal the dungeon with spikes and treasure."""
-        print '~' * self.columns * 2
-        for row in self.board:
-            print " ".join(row)
-        print '~' * self.columns * 2
-        print("The dungeon revealed!\n")
-
-    def add_spikes(self):
-        """Randomly add spikes."""
-        print("Adding spikes in random places... You know, for safety...")
-        for row in range(self.rows):
-            for column in range(self.columns):
-                if random.random() < 0.5:
-                    self.board[row][column] = '*'
-        time.sleep(1)
-
-        print("Done, now you can hurt yourself here.\n")
-
-    def create_treasure(self):
-        """Randomly add treasure."""
-        print("Hiding treasure... Because without treasure there is no fun at all.")
-        self.board[random.randrange(self.rows)][random.randrange(self.columns)] = 'T'
-        time.sleep(0.7)
-
-        print("Try to find it.\n")
-
-
 def check_if_number(user_input):
     """Check that user's input is a number"""
     while type(user_input) != int:
         try:
             user_input = int(user_input)
         except ValueError:
-            user_input = raw_input("Hm... I guess you have problems with numbers.\n"
-                                   "Try one more time, remember! This are numbers -> 123456789, in case you forgot.\n"
-                                   "Now, type the number: ")
+            user_input = input("Hm... I guess you have problems with numbers.\n"
+                               "Try one more time, remember! This are numbers -> 123456789, in case you forgot.\n"
+                               "Now, type the number: ")
     return user_input
 
 
-dungeon_size = check_if_number(raw_input('Please create your dungeon, what size should it be? Type one number: '))
-if dungeon_size <= 1:
-    print('This is not cool size, let`s pretend you typed 5.\n')
-    dungeon_size = 5
+class Dungeon:
+    """Create Dungeon object."""
 
-dungeon = Dungeon(dungeon_size, dungeon_size)
+    def __init__(self, rows, columns):
+        self.board = []
+        self.columns = columns
+        self.rows = rows
+        for row in range(self.rows):
+            self.board.append(['-'] * self.columns)
+        for row in self.board:
+            print(" ".join(row))
+        print("Here is your dungeon! Look, how good it is!\n")
 
-dungeon.add_spikes()
-dungeon.create_treasure()
+    def __repr__(self):
+        return "Board size {}x{}\n".format(self.rows, self.columns)
 
-while True:
-    # choose square
-    user_row = check_if_number(raw_input("Please type row number from 1 to {}: ".format(dungeon.rows)))
-    user_column = check_if_number(raw_input("Please type column number from 1 to {}: ".format(dungeon.columns)))
+    def reveal(self):
+        """reveal the dungeon with spikes and treasure."""
+        print('~' * self.columns * 2)
+        for row in self.board:
+            print(" ".join(row))
+        print('~' * self.columns * 2)
+        print("The dungeon revealed!\n")
 
-    # check that selected square in the dungeon otherwise choose square again
-    if user_row > dungeon.rows or user_row < 0 or user_column > dungeon.columns or user_column < 0:
-        print("Please try to stay in dungeon. You need this treasure really much!\n")
-        continue
+    def player_map(self):
+        """shows current map"""
+        print("Dungeon map:")
+        print('~' * self.columns * 2)
+        for row in self.board:
+            hidden_row = []
+            for cell in row:
+                if cell == '*' or cell == 'T':
+                    hidden_row.append('-')
+                else:
+                    hidden_row.append(cell)
+            print(" ".join(hidden_row))
+        print('~' * self.columns * 2)
+        print()
 
-    if dungeon.board[user_row - 1][user_column - 1] == "*":
-        print("You are dead buddy :(\n")
-        dungeon.show()
-        break
-    elif dungeon.board[user_row - 1][user_column - 1] == "T":
-        print("You found a T letter. You won by the way.\nBye.")
-        dungeon.show()
-        break
+    def add_spikes(self):
+        """Randomly add spikes."""
+        print("Adding spikes in random places... You know, for safety...")
+        for row in range(self.rows):
+            for column in range(self.columns):
+                if random.random() < 0.3:
+                    self.board[row][column] = '*'
+        time.sleep(1)
+
+        print("...Done, now you can hurt yourself here. Much fun!\n")
+
+    def create_treasure(self):
+        """Randomly add treasure."""
+        print("Hiding treasure... Because without treasure there is no fun at all.")
+        self.board[random.randrange(self.rows)][random.randrange(self.columns)] = 'T'
+        time.sleep(1)
+
+        print("...Now, try to find it!\n")
+
+game_on = True
+while game_on:
+    dungeon_size = check_if_number(input("Please create your dungeon, what size should it be? Type one number: "))
+    print()
+    if dungeon_size <= 2:
+        print("This is not cool size, let`s pretend you typed 5.\n")
+        dungeon_size = 5
+
+    dungeon = Dungeon(dungeon_size, dungeon_size)
+
+    dungeon.add_spikes()
+    dungeon.create_treasure()
+
+    while True:
+        # choose square
+        user_row = check_if_number(input("Please type row number from 1 to {}: ".format(dungeon.rows)))
+        user_column = check_if_number(input("Please type column number from 1 to {}: ".format(dungeon.columns)))
+        print()
+
+        # check that selected square in the dungeon otherwise choose square again
+        if user_row > dungeon.rows or user_row < 0 or user_column > dungeon.columns or user_column < 0:
+            print("Please try to stay in dungeon. You need this treasure really much!\n")
+            continue
+
+        if dungeon.board[user_row - 1][user_column - 1] == "*":
+            time.sleep(0.3)
+            print("SPIKES!!!\nYou are dead buddy :(\n")
+            dungeon.reveal()
+            break
+        elif dungeon.board[user_row - 1][user_column - 1] == "T":
+            time.sleep(0.3)
+            print("You found a T letter. You won by the way.\nBye.")
+            dungeon.reveal()
+            break
+        else:
+            time.sleep(0.3)
+            print("Ok there is no spikes here and no treasures, keep going bro!\n")
+            dungeon.board[user_row - 1][user_column - 1] = "0"
+            dungeon.player_map()
+            continue
+
+    restart = input("To start new game type any kind word or press enter to quit this EPIC GAME: ")
+    print()
+    if restart:
+        game_on = True
     else:
-        print("Ok there is no spikes here and no treasures, keep going bro!\n")
+        game_on = False
+        print("See you soon young adventurer! Bye!")
